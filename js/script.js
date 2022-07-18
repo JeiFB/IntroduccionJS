00b1829f20fa1d69d5fb4029eb7a8dfdf413d7a4
JeiFB <fuentesjeifer@gmail.com>
2022-07-18 17:11:21 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//variables
+var producto = 'audifonos gamer'; //iniciar variable y asignarle valor
+
+var disponible; //iniciamos la variable pero sin valor;
+
+// producto = true; //reasignamos el valor de la variable;
+
+disponible = true;
+
+var producto1 = 'computadora',
+    disponible1 = true,
+    categoria = 'computadora';
+
+
+//estilos para variables;
+
+var nombre_producto = 'Monitor HD'; //undercase
+var nombreProducto = 'Monitor Hd'; // camelcase
+var NombreProducto = 'Monitor Hd'; // pascal case
+var nombreproducto = 'Monitor Hd';
+
+// mostrar en consola
+
+console.log(producto);
